22bdbeffb47beced2973c669902c3bfd7027b554
jiylee <jjenny4711@gmail.com>
2023-05-29 15:12:51 -0400

    fixed config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const DB_URI = (process.env.NODE_ENV === "test")
 ? "postgresql:///manna_test"
-:"postgresql:///manna";
+: process.env.DATABASE_URL;
 
 const SECRET_KEY = process.env.SECRET_KEY || "jenny4711";
 const PORT = +process.env.PORT || 3001;
